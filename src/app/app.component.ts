a54abaf2c2e1bf8344a0ee856f8ed56433745921
mmpearce <mary.milburn.pearce@gmail.com>
2019-05-17 16:16:50 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ninja-gold';
+  title = 'ninja-gold rocks!';
+  pointVals = { 
+    farm : {min: 5, max: 10},
+    cave : {min: 10, max: 20},
+    house : {min: 2, max: 5},
+    casino : {min: -50, max: 50}
+  }
+  textColor: string;
+  gold: number = 0;
+  activities = [];
+
+  constructor(private _httpService: HttpService){}
+
+  ngOnInit() {
+    
+  }
+  onFindGold(location) {
+    let act = "";
+    let earned = this.pointVals[location]['min'] + (Math.floor(Math.random() * ((this.pointVals[location]['max']) - this.pointVals[location]['min'])));
+    this.gold = this.gold + earned;
+    let dt = new Date().toLocaleString();
+    if (earned < 0) {
+      act = `You lost ${0-earned} golds at the ${location}! (${dt})`
+      this.activities.push({ color: 'red', message: act });
+    }
+    else {
+      act = `You earned ${earned} golds at the ${location}! (${dt})`;
+      this.activities.push({ color: 'green', message: act });
+    }
+  }
 }
